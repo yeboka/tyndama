0cb4941349c01b49d4539caac5182a22fe2af74a
Yeboka <89529214+Yeboka@users.noreply.github.com>
2023-05-14 00:46:31 +0600

    commit

--- tyndama/models.py
+++ tyndama/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Music(models.Model):
+    song_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    singer = models.CharField(max_length=255)
+    tags = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images_1')
+    song = models.FileField(upload_to='images_1')
+
+    def _str_(self):
+        return self.name
