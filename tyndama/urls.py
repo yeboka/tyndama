84f0915154f204281781d9675dc093779c225712
MukagaliSDU <119991483+MukagaliSDU@users.noreply.github.com>
2023-05-14 01:26:50 +0600

    add new example file: get_music.html

--- tyndama/urls.py
+++ tyndama/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('get_music', views.get_music, name='get_music'),
 ]
--- tyndama/views.py
+++ tyndama/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from .models import Music
+from mutagen.mp3 import MP3
 # Create your views here.
 def home(request):
     return render(request, 'tyndama/home.html')
+
+
+def get_music(request):
+    music = Music.objects.all()
+    for i in music:
+        name = str(i.song)
+        audio_path = './media/' + name
+        print('this is url:: '+audio_path)
+        audio = MP3(audio_path)
+        length=int(audio.info.length)
+        print(length)
+
+    return render(request, 'tyndama/get_music.html', {'music': music})
