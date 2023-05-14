053e9b70ea1ac5ef704e87261e4eb2815b86ec79
MukagaliSDU <119991483+MukagaliSDU@users.noreply.github.com>
2023-05-15 03:45:33 +0600

    add import for urls

--- tyndama/urls.py
+++ tyndama/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
--- tyndama/views.py
+++ tyndama/views.py
@@ -11,9 +11,6 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
 
-from django.contrib.auth.models import User
-
-
 
 @login_required(login_url=login)
 def home(request):
