a3dd5ae312a4d01b78e6ba37e003468437d1d77d
MukagaliSDU <119991483+MukagaliSDU@users.noreply.github.com>
2023-05-15 10:56:51 +0600

    search added

--- tyndama/urls.py
+++ tyndama/urls.py
@@ -5,10 +5,10 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('get_music', views.get_music, name='get_music'),
     path('register/', views.registerPage, name='register'),
-    path('login/', views.login_page, name='login'),
+    path('', views.login_page, name='login'),
     path('logout/', views.logout_page, name='logout'),
     path('add_music/', views.add_music, name='add_music'),
     path('add_playlist/', views.add_playlist, name='add_playlist'),
@@ -17,4 +17,5 @@ urlpatterns = [
     path('playlist/<int:playlist_id>/', views.playlist_detail, name='playlist'),
     path('admin_panel/', views.admin_panel, name='admin_panel'),
     path('add_to_playlist/', views.add_to_playlist, name='add_to_playlist'),
+
 ]
