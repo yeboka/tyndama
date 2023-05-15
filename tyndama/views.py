d894592dea0f779a4bd183509cc63c8b5cd3a8e5
Aidana <210103080@stu.sdu.edu.kz>
2023-05-15 11:33:10 +0600

    add new remove method

--- tyndama/views.py
+++ tyndama/views.py
@@ -116,7 +116,10 @@ def add_music(request):
             instance = form.save(commit=False)
             instance.save()
             messages.success(request, 'Form is valid!')
-            return redirect('home')
+            if request.user.is_superuser:
+                return redirect('admin_panel')
+            else:
+                return redirect('user_profile')
         else:
             print('error')
     context = {'form': form}
@@ -172,7 +175,7 @@ def delete_music(request, pk):
         return render(request, 'tyndama/delete_music.html', context)
 
     music.delete()
-    return redirect('home')
+    return redirect('admin_panel')
 
 
 def add_to_playlist(request):
