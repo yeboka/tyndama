387f0919b2f22f8f65fd19edaeac53197c8328c8
Yeboka <89529214+Yeboka@users.noreply.github.com>
2023-05-15 06:51:21 +0600

    clean urls.py

--- tyndama/urls.py
+++ tyndama/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
+
 from . import views
-from django.urls import include, path
-from django.contrib.auth import views as auth_views
-
-
 
 urlpatterns = [
     path('', views.home, name='home'),
